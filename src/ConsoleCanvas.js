a9e124b6d8a7e26a57d4edb7031905bea89c9ea2
ninofiliu <nino.filiu@telecom-paristech.fr>
2019-11-06 21:04:39 +0100

    add Pixel (empty)

--- src/ConsoleCanvas.js
+++ src/ConsoleCanvas.js
@@ -1,6 +1,13 @@
+const Pixel = require('./Pixel');
+
 module.exports = class ConsoleCanvas {
     constructor(width, height) {
         this.width = width;
         this.height = height;
+        this.screen = (new Array(height)).fill().map(() => (
+            (new Array(width)).fill().map(() => (
+                new Pixel()
+            ))
+        ));
     }
 };
---
+++ src/Pixel.js
@@ -0,0 +1,7 @@
+const DEFAULT_PIXEL_LETTER = ' ';
+
+module.exports = class Pixel {
+    constructor() {
+        this.letter = DEFAULT_PIXEL_LETTER;
+    }
+};
